bb148a9cef859617f9d4ed3cf7aaceb628ee57ec
Dev-RMV <71718060+Dev-RMV@users.noreply.github.com>
2023-12-15 23:37:35 -0300

    https://atrmv.azurewebsites.net/       **AT Finalizado**

--- TP3.NET.MVC/Data/MasterContext.cs
+++ TP3.NET.MVC/Data/MasterContext.cs
@@ -27,7 +27,10 @@ namespace TP3.NET.MVC.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Master;Trusted_Connection=True;");
+                //local
+                //optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Master;Trusted_Connection=True;");
+                //azure
+                optionsBuilder.UseSqlServer("Server=tcp:rmv.database.windows.net,1433;Initial Catalog=rmv;Persist Security Info=False;User ID=rmv123;Password=Admin123*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
